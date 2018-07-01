936693b5ba8c6e453a78f76dd5af693bd011cb2a
ckattenberg <celine.kattenberg@hotmail.com>
2018-07-01 17:40:59 +0200

    Update chatbot_interface.py

--- chatbot_interface.py
+++ chatbot_interface.py
@@ -5,7 +5,6 @@ one dictionary for each URL returned by Watson discovery.
 """
 
 # Imports
-
 import os
 import json
 import nltk
@@ -15,6 +14,7 @@ from watson_developer_cloud import DiscoveryV1
 
 # Helper functions
 
+# Extracts the passage with the highest score for each of the pages returned by Watson
 def get_passage(passages_list, doc_id):
     for passage_dict in passages_list:
         if passage_dict['document_id'] == doc_id:
@@ -22,26 +22,29 @@ def get_passage(passages_list, doc_id):
 
     return("No passage was found for this document.")
 
+# Adds a bonus if page is from the right level in the document hierarchy
 def add_bonus(s, p):
     m = 1 - s
     b = p * m
     return(s+b)
 
-
-
+# Interface
 def frontend_backend_interface(input_dict):
     discovery = DiscoveryV1(
         version="2018-03-05",
         username="044cea39-4515-404b-a5c1-3e7b216e1635",
         password="RK5rIKZ01yIf"
     )
+    # Get the natural language query
     user_query = input_dict["Source"]
-
+    
+    # See if the query is in Dutch or in English
     if input_dict['Language'] == 0: # Nederlands
         collection_id = '6e027bae-6639-4815-9189-08ba8518f65f'
     else:
         collection_id = 'fb1465fc-5731-4e56-9028-fac58374b638' # Engels
-
+        
+    # Send the query to the Watson discovery service with the discovery API
     my_query = discovery.query(environment_id='26410a73-bf30-47c5-85cb-538a14797fd2',
                                 collection_id=collection_id,
                                 natural_language_query=user_query, return_fields=['text', 'level', 'keywords','url'],
@@ -52,6 +55,7 @@ def frontend_backend_interface(input_dict):
 
     qr1 = json.loads(json.dumps(my_query, indent=2))
 
+    # Create the output list of dictionaries (1 dictionary for each returned page)
     output_list = []
     # Add the dictionaries
     pages_dict = qr1['results'] #from here we get the url and scores
@@ -62,8 +66,6 @@ def frontend_backend_interface(input_dict):
         else:
             confidence = qr1['results'][i]['result_metadata']['score']
             confidence = (confidence/(4+confidence))
-
-
             
         level =  qr1['results'][i]['level']
         url = qr1['results'][i]['url']
@@ -72,6 +74,7 @@ def frontend_backend_interface(input_dict):
         if input_dict["Level"] in level:
             confidence = add_bonus(confidence, 0.5)
 
+        # Create dictionary with the url, score, level, keywords and document id of the page
         page_dict = {"URL": url, "Score": confidence, "Level": level, "Keywords": keywords, "ID": doc_id}#, "Answer": passage}
 
         output_list.append(page_dict)
@@ -83,6 +86,7 @@ def frontend_backend_interface(input_dict):
         page.pop('ID', None)
         page["Answer"] = passage
 
+    # Sort the list based on confidence score
     output_list = sorted(output_list, key=lambda k: k['Score'], reverse=True)
 
     return(output_list)
