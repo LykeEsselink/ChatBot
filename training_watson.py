aee090d71f293b4df0ac597a0f012a27241574f8
ckattenberg <celine.kattenberg@hotmail.com>
2018-07-01 17:47:29 +0200

    Update training_watson.py

--- training_watson.py
+++ training_watson.py
@@ -5,12 +5,14 @@ The relevance of the results must then be rated manually and submitted to watson
 with the send_relevancy function. The example queries come from the watson_questions.txt file.
 """
 
+# Imports
 import os
 import json
 import nltk
 import requests
 from watson_developer_cloud import DiscoveryV1
 
+# Sends natural language query to the watson discovery service with the discovery API
 def send_query(user_query):
     discovery = DiscoveryV1(
         version="2018-03-05",
@@ -29,32 +31,12 @@ def send_query(user_query):
 
 
     qr1 = json.loads(json.dumps(my_query, indent=2))
-    #print(qr1['matching_results'])
-    #print(qr1['passages'][0])
     st = []
     for dicts in qr1['passages']:
         st.append(['QUERY: ' + user_query, 'SCORE: ' + str(dicts['passage_score']), 'TEXT: ' + dicts['passage_text'], 'DOC ID:' + dicts['document_id']])
     return(st)
 
-wq = open('watson_questions').read()
-wq = nltk.sent_tokenize(wq)
-for questions in wq:
-    print(questions)
-    print('\n')
-
-# GEBRUIK DEZE VOOR HET BEPALEN VAN RELEVANCY, HIER ZITTEN DE TEKSTEN EN DOCUMENT IDs IN
-query_responses = [None] * 52
-for i, sents in enumerate(wq):
-    query_responses[i] = send_query(sents)
-
-
-# PASSAGES ---- PASSAGES ---- PASSAGES ---- PASSAGES ---- PASSAGES ---- PASSAGES ---- PASSAGES ---- PASSAGES ---- # PASSAG
-for questions in query_responses:
-    print("------------------------------------------------------------------------------------------")
-    for stuff in questions:
-        print("\n")
-        print(stuff)
-
+# Can be used to rate watson's responses to train the model
 def send_relevancy(query, articleid1, relevancy1, articleid2, relevancy2):
     headers = {
     'Content-Type': 'application/json',
@@ -68,3 +50,25 @@ def send_relevancy(query, articleid1, relevancy1, articleid2, relevancy2):
     print(data)
     response = requests.post('https://gateway.watsonplatform.net/discovery/api/v1/environments/26410a73-bf30-47c5-85cb-538a14797fd2/collections/fb1465fc-5731-4e56-9028-fac58374b638/training_data', headers=headers, params=params, data=data, auth=('044cea39-4515-404b-a5c1-3e7b216e1635', 'RK5rIKZ01yIf'))
     return(response.content)
+
+# Open the training questions and print them out
+wq = open('watson_questions').read()
+wq = nltk.sent_tokenize(wq)
+for questions in wq:
+    print(questions)
+    print('\n')
+
+# Creates a list of responses for the training queries
+query_responses = [None] * 52
+for i, sents in enumerate(wq):
+    query_responses[i] = send_query(sents)
+
+# Print all the responses so they can be rated manually
+for questions in query_responses:
+    print("------------------------------------------------------------------------------------------")
+    for stuff in questions:
+        print("\n")
+        print(stuff)
+
+
+
